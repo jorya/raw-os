adee210ed4af0b7b761ee525dc6fd5f0dd7eca19
jorya <jorya.txj@gmail.com>
2014-07-03 12:51:20 +0800

    add raw-os.c to improve both compiling and running speed.

---
+++ raw-os.c
@@ -0,0 +1,55 @@
+/*
+     raw os - Copyright (C)  Lingjun Chen(jorya_txj).
+
+    This file is part of raw os.
+
+    raw os is free software; you can redistribute it it under the terms of the 
+    GNU General Public License as published by the Free Software Foundation; 
+    either version 3 of the License, or  (at your option) any later version.
+
+    raw os is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+    without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program. if not, write email to jorya.txj@gmail.com
+                                      ---
+
+    A special exception to the LGPL can be applied should you wish to distribute
+    a combined work that includes raw os, without being obliged to provide
+    the source code for any proprietary components. See the file exception.txt
+    for full details of how and when the exception can be applied.
+*/
+
+
+/* 	2014-7  Created by jorya_txj
+  *	xxxxxx   please added here
+  */
+
+#include "raw_obj.c"
+#include "raw_sched.c"
+#include "raw_pend.c"
+#include "raw_task.c"
+#include "raw_task_0.c"
+#include "raw_block.c"
+#include "raw_byte.c"
+#include "raw_event.c"
+#include "raw_idle.c"
+#include "raw_idle_event.c"
+#include "raw_mqueue.c"
+#include "raw_mutex.c"
+#include "raw_queue.c"
+#include "raw_queue_buffer.c"
+#include "raw_queue_size.c"
+#include "raw_sem.c"
+#include "raw_stat.c"
+#include "raw_stm.c"
+#include "raw_system.c"
+#include "raw_task_queue_size.c"
+#include "raw_task_sem.c"
+#include "raw_tick.c"
+#include "raw_timer.c"
+
+
+
+
