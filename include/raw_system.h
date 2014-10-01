0f8480c26817123a63e6762035b71458be4f4083
jorya <jorya.txj@gmail.com>
2014-10-01 09:49:39 +0800

    update to version 1057

--- include/raw_system.h
+++ include/raw_system.h
@@ -28,9 +28,9 @@
 #ifndef RAW_SYSTEM_H
 #define RAW_SYSTEM_H
 
-#define RAW_OS_VERSION                              1056
+#define RAW_OS_VERSION                              1057
 
-#define RAW_OS_BUILD_TIME                           "2014/08/21"     
+#define RAW_OS_BUILD_TIME                           "2014/10/1"     
 
 #define IDLE_PRIORITY                               (CONFIG_RAW_PRIO_MAX - 1)
 
