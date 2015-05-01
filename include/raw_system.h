28d4e20d42cc609faa86ab70146b48b6ef068c2c
txj <57026783@qq.com>
2015-05-01 09:50:39 +0800

    update to version 1080

--- include/raw_system.h
+++ include/raw_system.h
@@ -28,9 +28,9 @@
 #ifndef RAW_SYSTEM_H
 #define RAW_SYSTEM_H
 
-#define RAW_OS_VERSION                              1060
+#define RAW_OS_VERSION                              1080
 
-#define RAW_OS_BUILD_TIME                           "2015/04/28"     
+#define RAW_OS_BUILD_TIME                           "2015/05/01"     
 
 #define IDLE_PRIORITY                               (CONFIG_RAW_PRIO_MAX - 1)
 
