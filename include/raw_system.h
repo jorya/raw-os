17cfc6b6bcc3743f7dc63f56933b4dbc3aeeb58d
txj <57026783@qq.com>
2015-08-01 11:51:58 +0800

    update to 1090

--- include/raw_system.h
+++ include/raw_system.h
@@ -28,9 +28,9 @@
 #ifndef RAW_SYSTEM_H
 #define RAW_SYSTEM_H
 
-#define RAW_OS_VERSION                              1081
+#define RAW_OS_VERSION                              1090
 
-#define RAW_OS_BUILD_TIME                           "2015/06/19"     
+#define RAW_OS_BUILD_TIME                           "2015/08/01"     
 
 #define IDLE_PRIORITY                               (CONFIG_RAW_PRIO_MAX - 1)
 
