203988669673f0da46be533d3fb38ae7bf25c835
txj <57026783@qq.com>
2015-10-18 11:57:45 +0800

    update to version 1110

--- include/raw_system.h
+++ include/raw_system.h
@@ -28,9 +28,9 @@
 #ifndef RAW_SYSTEM_H
 #define RAW_SYSTEM_H
 
-#define RAW_OS_VERSION                              1101
+#define RAW_OS_VERSION                              1110
 
-#define RAW_OS_BUILD_TIME                           "2015/10/09"     
+#define RAW_OS_BUILD_TIME                           "2015/10/18"     
 
 #define IDLE_PRIORITY                               (CONFIG_RAW_PRIO_MAX - 1)
 
